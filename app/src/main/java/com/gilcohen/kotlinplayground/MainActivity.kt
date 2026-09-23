418f76cb8531c7752e4870d1161cf428e8125aad
COHEN\gilco <gilcohen7@gmail.com>
2026-09-23 10:20:58 +0300

    Test push 2

--- app/src/main/java/com/gilcohen/kotlinplayground/MainActivity.kt
+++ app/src/main/java/com/gilcohen/kotlinplayground/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     KotlinPlaygroundTheme {
-        Greeting("Androidd")
+        Greeting("Android")
     }
 }
